518a000d0d9877d95f4944b9934945b17d6a873a
Sambo Chea <sombochea100@gmail.com>
2019-05-27 14:32:36 +0700

    updated pages

---
+++ precache-manifest.09a089a04dd4049ebd42f13c5fe79d26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb5a0a8abd4dd210d06f905b730c2706",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dec390725ab84afc66b",
+    "url": "/static/css/main.3bf074fd.chunk.css"
+  },
+  {
+    "revision": "ed3414b097ec5d5000be",
+    "url": "/static/js/2.f7844318.chunk.js"
+  },
+  {
+    "revision": "2dec390725ab84afc66b",
+    "url": "/static/js/main.48d5bf55.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.668b433d65a542c5b50890ea96be8cc1.js"
+  "/precache-manifest.09a089a04dd4049ebd42f13c5fe79d26.js"
 );
 
 self.addEventListener('message', (event) => {
